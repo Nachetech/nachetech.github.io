341b0392a48de1881fe9f9371aafe06798c6b84d
Nachetech <122560786+Nachetech@users.noreply.github.com>
2023-02-26 18:25:18 +0100

    Update multiMarkersname.js

--- js/multiMarkersname.js
+++ js/multiMarkersname.js
@@ -7,14 +7,14 @@ AFRAME.registerComponent('markers_start',{
     var sceneEl = document.querySelector('a-scene');
     
     //lists of the markers
-    for(var i=1; i<6; i++)
+    for(var i=1; i<18; i++)
     {
       var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
       markersURLArray.push(url);
       markersNameArray.push('Marker_'+i);      
     }
 
-    for(var k=0; k<6; k++)
+    for(var k=0; k<18; k++)
     {
       var markerEl = document.createElement('a-marker');
       markerEl.setAttribute('type','pattern');
@@ -31,7 +31,7 @@ AFRAME.registerComponent('markers_start',{
        animationEl.setAttribute('gltf-model', 'assets/animation' + (k+1) + '.glb');
        animationEl.setAttribute('animation', {property: 'rotation', to: '360 360 360', loop: 'repeat', dur: '5000'});
        animationEl.setAttribute('animation-mixer', '');
-       animationEl.setAttribute('scale', '0.01 0.01 0.01');
+     
 
      // Setting the position of the animation
        animationEl.object3D.position.set(0, 0, 0);
